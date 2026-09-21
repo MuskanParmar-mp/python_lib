d0f1c40548b814b780cd7ed166663f4baa0c3dc3
MuskanParmar-mp <parmarmuskan1362006@gmail.com>
2026-09-21 10:50:54 +0530

    plib

---
+++ new python lib/slicing.py
@@ -0,0 +1,49 @@
+# indexing
+
+
+'''a = [10, 20, 30, 40, 50]
+
+print(a[0])   # 10
+print(a[1])   # 20
+print(a[2])   # 30
+print(a[4])   # 50
+
+
+
+# ------------------. Negative Indexing-----------------------------
+
+a = [10, 20, 30, 40, 50]
+print(a[-1])                  #last element
+print(a[-2])
+
+
+# -------------------Indexing with len()
+
+a = [10, 20, 30, 40, 50]
+print(len(a))                 # 5
+print(a[len(a)-1])            # 50
+
+
+s = "python language"
+print(s.index(" "))
+
+print(s[6:-9])
+print(s.index("a"))
+print(s.index("yth"))'''
+
+
+
+
+
+
+
+#---------------------------------------------------------
+
+text = "python"
+print(text[0:4])
+print(text[2:5])
+print(text[:3])
+print(text[3:0])
+print(text[::2])
+print(text[::-1])
+print(text[-2:-4])
